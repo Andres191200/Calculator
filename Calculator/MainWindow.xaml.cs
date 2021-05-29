1d3b99094abe8b90c0da8fb8387740b883db3f3f
Andres <marianoandres191200@gmail.com>
2021-05-29 16:32:53 -0300

    Calculator and operators interface added

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        float result = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private double Button_Click_0(object sender, RoutedEventArgs e)
+        {
+            return 0;
+        }
+
+        private double Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            return 1;
+        }
+
+        private double Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            return 2;
+        }
+
+        private double Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            return 3;
+        }
+
+        private double Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            return 4;
+        }
+        private double Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            return 5;
+        }
+        private double Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            return 6;
+        }
+        private double Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            return 7;
+        }
+        private double Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            return 8;
+        }
+        private double Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            return 9;
+        }
+
+
+        //OPERATOR FUNCTIONS
+        private void Divide_Button_Click(object sender, RoutedEventArgs e, float num)
+        {
+            result /= num;
+            
+        }
+
+        private void Adding_Button_Click(object sender, RoutedEventArgs e, float num)
+        {
+            result += num;
+        }
+
+        private void Multiply_Button_Click(object sender, RoutedEventArgs e, float num)
+        {
+            result *= num;
+        }
+
+        private void Substract_Button_Click(object sender, RoutedEventArgs e, float num)
+        {
+            result -= num;
+        }
+
+        public float GetResult(object sender, RoutedEventArgs e)
+        {
+            return result;
+        }
+    }
+}
